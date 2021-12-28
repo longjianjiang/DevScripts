fe7248ae940857fe59d2c7e0a1d81eb631f5dc40
jianglongjian <longjian.jiang@ximalaya.com>
2021-12-28 16:18:56 +0800

    add update pod spec script

---
+++ updatePodspec/action.rb
@@ -0,0 +1,124 @@
+# 判断当前分支，判断工作区是否为空，否则不允许操作；
+# 当前路径，查找是否存在podspec；
+# 读取到version，打印出来；
+# 输入新到version；
+# 更改podspec，提交，打tag，推送；
+
+require 'tempfile'
+
+class Color
+    def self.red
+        31
+    end
+    def self.green
+        32
+    end
+    def self.white
+        37
+    end
+end
+
+def color_text(text, color = Color.natural)
+    if color == 0
+        return text
+    end
+    return "\033[#{color}m#{text}\033[0m"
+end
+
+# 1
+current_branch = `git rev-parse --abbrev-ref HEAD`.strip
+target_branchs = ["master", "main"]
+if target_branchs.include?(current_branch) == false
+  error_msg = "Error: Current branch is not #{target_branchs.inspect}."
+  puts color_text(error_msg, Color.red)
+  exit
+end
+
+# 2
+git_status_output = `git status --porcelain`
+if git_status_output.length > 0 
+  error_msg = "Error: Changes not staged for commit."
+  puts color_text(error_msg, Color.red)
+  exit
+end
+
+# 3
+accepted_files = [".podspec"]
+podspec_dir = Dir.pwd
+podspec_file_name = ""
+for file_name in Dir.children(podspec_dir) do
+  if accepted_files.include?(File.extname(file_name)) 
+    podspec_file_name = file_name
+    break
+  end
+end
+if podspec_file_name.length == 0 
+  error_msg = "Error: Not found podspec file."
+  puts color_text(error_msg, Color.red)
+  exit
+end
+
+# 4
+podspec_file_path = File.join(podspec_dir, podspec_file_name)
+current_version = ""
+podspec_file = File.open(podspec_file_path)
+podspec_file_data = podspec_file.read
+podspec_file_data.each_line do |line|
+  key_value = line.split('=')
+  if (key_value.length == 2)
+    key = key_value.first.to_s.gsub("\n", '').gsub(' ','').gsub("\t",'')
+    if key == "s.version"
+      value = key_value.last.to_s.gsub("\n", '').gsub(' ','').gsub("\t",'')
+      current_version = value
+      break
+    end
+  end
+end
+if current_version.length == 0
+  error_msg = "Error: Not found prev version."
+  puts color_text(error_msg, Color.red)
+  exit
+end
+
+# 5
+puts color_text("Current version is #{current_version}", Color.white)
+puts "Enter new version: "
+new_version = gets.chomp
+## version valid check;
+
+# 6
+temp_podspec_file_name = "temp.podspec"
+temp_podspec_file = Tempfile.new(temp_podspec_file_name)
+begin
+  podspec_file_data.each_line do |line|
+      to_write_line = line
+      key_value = line.split('=')
+      if (key_value.length == 2)
+        original_key = key_value.first.to_s
+        key = key_value.first.to_s.gsub("\n", '').gsub(' ','').gsub("\t",'')
+        if key == "s.version"
+          to_write_line = original_key + "= #{new_version}"
+        end
+      end
+
+      temp_podspec_file.puts to_write_line 
+  end
+  temp_podspec_file.close
+  FileUtils.mv(temp_podspec_file.path, podspec_file_path)
+ensure
+  temp_podspec_file.close
+  temp_podspec_file.unlink
+end
+podspec_file.close
+
+# 7
+add_command = "git add #{podspec_file_name}"
+commit_msg = "new version #{new_version}"
+commit_command = "git commit -m '#{commit_msg}'"
+tag_command = "git tag '#{new_version}' "
+
+`#{add_command}`
+`#{commit_command}`
+`#{tag_command}`
+
+puts color_text("Success.", Color.green)
